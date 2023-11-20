63ce442adf95245609d57b4eb7f326a3ed0edead
srinivasulareddy544 <sreenureddylucky702@gmail.com>
2023-11-20 22:41:35 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sai(request):
+  return render(request,'sai.html')
+def sreenu(request):
+  return render(request,'sreenu.html')
+def vishnu(request):
+  return render(request,'vishnu.html')
